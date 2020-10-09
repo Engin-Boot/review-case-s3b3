c116a4018f67b4dd42f77e4ffa8812885f5364be
Harshis517 <56535203+Harshis517@users.noreply.github.com>
2020-10-09 13:36:16 +0530

    Update ReviewTest.java

--- src/test/java/com/philips/receiver/ReviewTest.java
+++ src/test/java/com/philips/receiver/ReviewTest.java
@@ -3,26 +3,28 @@ package com.philips.receiver;
 import static org.junit.Assert.*;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Set;
 import java.util.TreeSet;
 
+import org.junit.Assert;
 import org.junit.Test;
 
-public class ReceiverTest {
-
+public class ReviewTest {
 
 	@Test
 	public void TokenizerTestPassing() {
 		
 		Set<String> TestString1 = new TreeSet<String>();
-		TestString1 = Receiver.Tokenizer("check the tokenizer funtion");
+		TestString1 = Receiver.tokenizer("check the tokenizer funtion");
 		int count1 = TestString1.size();
 		
 		Set<String> TestString2 = new TreeSet<String>();
-		TestString2 = Receiver.Tokenizer("check, the, tokenizer, funtion, with, commas");
+		TestString2 = Receiver.tokenizer("check, the, tokenizer, funtion, with, commas");
 		int count2 = TestString2.size() ;
 		
 		assertEquals(4, count1);
@@ -31,8 +33,9 @@ public class ReceiverTest {
 	}
 	@Test
 	public void TokenizerTestFailing() {
+		
 		Set<String> TestString1 = new TreeSet<String>();
-		TestString1 = Receiver.Tokenizer("check the tokenizer funtion");
+		TestString1 = Receiver.tokenizer("check the tokenizer funtion");
 		int count1 = TestString1.size();
 		
 		assertNotEquals(2, count1);
@@ -43,14 +46,33 @@ public class ReceiverTest {
 	@Test	
 	public void test() throws FileNotFoundException {	
 
-		String wordCount = "provided";	
-		BufferedReader br = new BufferedReader(new FileReader("src//test//resources//senderSampleTest1.csv"));	
-		Receiver.countFrequency(wordCount);	
-		assertTrue(wordCount.contains("provided"));	
-		assertFalse(wordCount.contains("future"));	
-		assertFalse(wordCount.contains("today"));	
-	}
+		HashMap<String, Integer> wordCount = new HashMap<String, Integer>();	
+		BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\shubhanshu\\eclipse-workspace\\casestudy (1).zip_expanded\\casestudy.zip_expanded\\casestudyreview\\src\\test\\resources\\senderSampleTest1.csv"));	
+		Receiver.countFrequency(br,wordCount);
 		
+		HashMap<String, Integer> wordCount2 = new HashMap<String, Integer>();	
+		BufferedReader br2 = new BufferedReader(new FileReader("C:\\Users\\shubhanshu\\eclipse-workspace\\casestudy (1).zip_expanded\\casestudy.zip_expanded\\casestudyreview\\src\\test\\resources\\TestSample2.csv"));	
+		Receiver.countFrequency(br2,wordCount2);
+		assertTrue(wordCount.containsKey("provided"));	
+		assertFalse(wordCount2.containsKey("future"));	
+		assertFalse(wordCount2.containsKey("today"));	
+	}
+	
+	@Test
+	public void writeWordCountToCSVTest() throws IOException {
+		
+		HashMap<String, Integer> wordCount = new HashMap<String, Integer>();
+		wordCount.put("apple", 100);
+		wordCount.put("banana", 50);
+		wordCount.put("mango", 80);
+		wordCount.put("pineapple", 90);
+		wordCount.put("cherry", 200);
+		wordCount.put("guava", 20);
+		Receiver.writeWordCountToCSV(wordCount);
+		File dir = new File("C:\\Users\\shubhanshu\\eclipse-workspace\\casestudy (1).zip_expanded\\casestudy.zip_expanded\\casestudyreview\\src\\main\\java\\com\\philips\\receiver");
+		File[] files = dir.listFiles((dir1, name) -> name.startsWith("output") && name.endsWith(".csv"));
+		Assert.assertNotNull(files);
+	}
 	
 	
 }
