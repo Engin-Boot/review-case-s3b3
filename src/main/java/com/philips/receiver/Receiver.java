2ff37996e49d728e71479e0a851bdf1be986747d
Harshis517 <56535203+Harshis517@users.noreply.github.com>
2020-10-09 13:35:37 +0530

    Update Receiver.java

--- src/main/java/com/philips/receiver/Receiver.java
+++ src/main/java/com/philips/receiver/Receiver.java
@@ -1,25 +1,24 @@
 package com.philips.receiver;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.HashMap;
 import java.io.InputStreamReader;
-import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.StringTokenizer;
 import java.util.TreeSet;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
 
 public class Receiver {
 
-	private static HashMap<String, Integer> wordCount = new HashMap<String, Integer>();
+	private static HashMap<String, Integer> wordCount = new HashMap<String,Integer>();
 
 	public static void main(final String[] args) throws IOException {
-		BufferedReader reader=Receiver.readInputFromConsole(System.in);
-		
+
+		readFromconsole();
+
 		try {
 			writeWordCountToCSV(wordCount);
 		} catch (final Exception e) {
@@ -27,34 +26,20 @@ public class Receiver {
 		}
 	}
 
-	public static BufferedReader readInputFromConsole(InputStream in) {
-		try {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-		return reader;
-		}catch(NullPointerException e) {
-			System.out.println(e.getMessage());
-		}
-		return null;
-	}
-	
-	public static void parseLineFromCSV(BufferedReader reader)
-	{
-		try {
-			String reviewComment = reader.readLine();
-            while(reviewComment!=null)
-            {
-                countFrequency(reviewComment);
-                reviewComment=reader.readLine();
-            }
-        }catch(Exception e) {
-        	System.out.println(e.getMessage());
-        }
-	}
-	public static void countFrequency(String review) {
+	private static void readFromconsole() {
 
-				Set<String> wordSet ;
-				review = review.replaceAll("[\\p{Punct}&&[^/]]+", "").toLowerCase();
-				wordSet = Tokenizer(review);
+		final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		countFrequency(reader,wordCount);
+	}
+
+	public static void countFrequency(final BufferedReader reader,HashMap<String, Integer> wordCount) {
+
+		try {
+
+			String line;
+			while ((line = reader.readLine()) != null) {
+				line = line.replaceAll("[\\p{Punct}&&[^/]]+", "").toLowerCase();
+				Set<String> wordSet = tokenizer(line);
 				for (final String word : wordSet) {
 					if (!wordCount.containsKey(word)) {
 						wordCount.put(word, 1);
@@ -64,9 +49,13 @@ public class Receiver {
 					}
 				}
 
+			}
+		} catch (final IOException e) {
+			e.getMessage();
+		}
 	}
 
-	public static Set<String> Tokenizer(final String review) {
+	public static Set<String> tokenizer(final String review) {
 		final Set<String> setOfWordsInReview = new TreeSet<String>();
 		final StringTokenizer st = new StringTokenizer(review, " ");
 		while (st.hasMoreTokens()) {
@@ -77,25 +66,26 @@ public class Receiver {
 	}
 
 	public static void writeWordCountToCSV(final Map<String, Integer> wordCount) throws IOException {
-		Set<String> wordSet = new TreeSet<String>();
-		wordSet = wordCount.keySet();
+
+		 Set<String>wordSet = wordCount.keySet();
 		String str = "Word,Count\n";
+		StringBuilder bld = new StringBuilder();
+
 		for (final String word : wordSet) {
 			System.out.println(word + " : " + wordCount.get(word));
-			str = str + word + "," + Integer.toString(wordCount.get(word)) + "\n";
-		}
+			 bld.append(word + "," + Integer.toString(wordCount.get(word)) + "\n");
+			 str = str+bld.toString();
 
-		PrintWriter pw = null;
-		try {
-			pw = new PrintWriter(new File("output.csv"));
-		} catch (final FileNotFoundException e) {
-			e.getMessage();
-			e.printStackTrace();
-		
 		}
+		
+
+		PrintWriter pw = new PrintWriter(new File("output.csv"));
+	
+		
 		pw.println(str);
+		  
 		pw.flush();
 		pw.close();
+		  
 	}
 }
-
