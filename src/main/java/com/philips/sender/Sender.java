052a469d5be22b41d78c7797c252a150698663c8
Himanshu Chaudhary <45047527+silent18knight@users.noreply.github.com>
2020-10-09 16:22:57 +0530

    updated

--- src/main/java/com/philips/sender/Sender.java
+++ src/main/java/com/philips/sender/Sender.java
@@ -9,7 +9,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 
-class Sender {
+public class Sender {
 	private static Logger LOGGER = LogManager.getLogger(Sender.class);
 	public static final String DELIMITER = ",";
 
@@ -43,7 +43,12 @@ class Sender {
 			if (line.isEmpty()) {
 				continue;
 			}
-			readFileContentWhenColumnFilter(line, columnFilterIndex);
+			
+			String[] rowEntryDataArray = line.split(DELIMITER);
+			if (columnFilterIndex >= 0 && rowEntryDataArray.length != 1)
+				readFileContentWhenColumnFilter0(columnFilterIndex,rowEntryDataArray);
+			else if (columnFilterIndex == 1 && rowEntryDataArray.length != 1)
+				readFileContentWhenColumnFilter1(columnFilterIndex,rowEntryDataArray);
 		}
 
 	}
@@ -61,27 +66,21 @@ class Sender {
 		return columnFilterIndex;
 	}
 
-	private static void readFileContentWhenColumnFilter(String line, int columnFilterIndex) {
-		String[] rowEntryDataArray = line.split(DELIMITER);
-		//if (columnFilterIndex >= 0 && rowEntryDataArray.length != 1) {
-		if (rowEntryDataArray.length > columnFilterIndex  && rowEntryDataArray.length != 1)
-				LOGGER.info(rowEntryDataArray[columnFilterIndex]);
-		
-	 printLastColumn(line,columnFilterIndex,rowEntryDataArray);
-		
+	public static void readFileContentWhenColumnFilter0( int columnFilterIndex,String[] rowEntryDataArray) {
+
+		if (rowEntryDataArray.length > columnFilterIndex)
+			LOGGER.info(rowEntryDataArray[columnFilterIndex]);
+		System.out.println();
 	}
-		
 
-	private static void printLastColumn(String line, int columnFilterIndex,String[] rowEntryDataArray ) {
-	
-		if(columnFilterIndex == 1 ) {
-			for (String row : rowEntryDataArray) {
-				String space = "  ";
-				LOGGER.info(row + space);
-			}
-		}
-			System.out.println();
-		}
-	
+	public static void readFileContentWhenColumnFilter1( int columnFilterIndex,String[] rowEntryDataArray) {
 
+		for (String row : rowEntryDataArray) {
+			String space = "  ";
+			LOGGER.info(row + space);
+		}
+		System.out.println();
+	}
 }
+
+
