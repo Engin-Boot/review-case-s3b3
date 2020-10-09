c4cfbd477f7726846e35080d7706be0705e7dae5
Harshis517 <56535203+Harshis517@users.noreply.github.com>
2020-10-09 16:42:41 +0530

    Update SenderTest.java

--- src/test/java/com/philips/sender/SenderTest.java
+++ src/test/java/com/philips/sender/SenderTest.java
@@ -17,7 +17,7 @@ public class SenderTest {
 
 	@Test
 	public void readWhenFileIsEmpty() throws IOException {
-		String filename = "src//test//resources//senderSampleTest1.csv";
+		String filename = "src/test/resources/senderSampleTest1.csv";
 		BufferedReader br = new BufferedReader(new FileReader(filename));
 		Sender.readFileContentWithoutColumnFilter(br);	
 		assertFalse((filename).isEmpty());
@@ -52,7 +52,7 @@ public class SenderTest {
 	@Test	
 	public void readFileContentWithoutCoulumnFilterTest() throws IOException {	
 
-		BufferedReader br = new BufferedReader(new FileReader("src//test//resources//senderSampleTest1.csv"));	
+		BufferedReader br = new BufferedReader(new FileReader("src/test/resources/senderSampleTest1.csv"));	
 		com.philips.sender.Sender.readFileContentWithoutColumnFilter(br);			
 	}	
 
@@ -61,7 +61,7 @@ public class SenderTest {
 	public void readFileContentWithCoulumnFilterTest() throws NumberFormatException, IOException, ParseException {	
 		
 		String str = "Comments";	
-		BufferedReader reader = new BufferedReader(new FileReader("src//test//resources//senderSampleTest1.csv"));			
+		BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/senderSampleTest1.csv"));			
 		com.philips.sender.Sender.readFileContentWhenColumnFilterIfProvided(str, reader);	
 	}
 
